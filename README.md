2e665e16d068e664c73a2b1c46caad4e1aa514ca
Ken Chen <kenchen@microsoft.com>
2017-05-24 15:05:30 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# javawebappsample
+# A Simple Java Web App for Azure
+
+## Build
+```shell
+mvn package
+```
+
+## Run Locally
+```shell
+mvn jetty:run
+```
+To run in a different port
+```shell
+mvn jetty:run -Djetty.port=<your port>
+```
+
+## Debug Locally
+```shell
+set MAVEN_OPTS=-Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=8000,suspend=n
+mvn jetty:run
+```
+
+## Deploy to Azure Web App
+Rename the `.war` file in `target` folder to `ROOT.war` and upload it to your Azure Web App through Git or FTP.
---
+++ src/main/java/ms/kenchen/Calculator/CalculatorService.java
@@ -0,0 +1,45 @@
+package ms.kenchen.Calculator;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+@Path("/calculator")
+public class CalculatorService {
+    @GET
+    @Path("ping")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String ping() {
+        return "pong!";
+    }
+
+    @GET
+    @Path("add")
+    @Produces(MediaType.APPLICATION_JSON)
+    public CalculatorResponse Add(@QueryParam("x") int x, @QueryParam("y") int y) {
+        return new CalculatorResponse(x, y, x + y);
+    }
+
+    @GET
+    @Path("sub")
+    @Produces(MediaType.APPLICATION_JSON)
+    public CalculatorResponse Sub(@QueryParam("x") int x, @QueryParam("y") int y) {
+        return new CalculatorResponse(x, y, x - y);
+    }
+
+    @GET
+    @Path("mul")
+    @Produces(MediaType.APPLICATION_JSON)
+    public CalculatorResponse Mul(@QueryParam("x") int x, @QueryParam("y") int y) {
+        return new CalculatorResponse(x, y, x * y);
+    }
+
+    @GET
+    @Path("div")
+    @Produces(MediaType.APPLICATION_JSON)
+    public CalculatorResponse Div(@QueryParam("x") int x, @QueryParam("y") int y) {
+        return new CalculatorResponse(x, y, x / y);
+    }
+}
