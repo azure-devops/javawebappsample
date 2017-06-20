26b45596759dc5df9a0522b3f3b61de2407c348d
Ken Chen <kenchen@microsoft.com>
2017-06-20 22:54:39 +0800

    Update CalculatorService.java

--- src/main/java/ms/kenchen/Calculator/CalculatorService.java
+++ src/main/java/ms/kenchen/Calculator/CalculatorService.java
@@ -13,7 +13,7 @@ public class CalculatorService {
     @Path("ping")
     @Produces(MediaType.TEXT_PLAIN)
     public String ping() {
-        return "Welcome to Java Web App!\n" + new Date().toString();
+        return "Welcome to Java Web App!!!\n" + new Date().toString();
     }
 
     @GET
